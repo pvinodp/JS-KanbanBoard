153722663d85f8abaf10b373bdd3a834a3a7d15d
vinod prabhu <vinoprab@cisco.com>
2020-04-11 19:28:41 +0530

    kanban excercise.

--- js/kanban.js
+++ js/kanban.js
@@ -43,10 +43,27 @@ const create_item = () => {
             order = order +1;
             item.innerHTML = input.value;
             adding = false;
+      }else{
+        error.innerHTML = message;
       }
   });
+  item.appendChild(save_btn);
+  return item;
 };
 
 
 document.querySelectorAll('.drop').forEach(element => {
+
+    element.addEventListener("drop" , (event) =>{
+
+        event.preventDefault();
+        const id = event.dataTransfer.getData('text');
+        event.target.appendChild(document.getElementById(id));
+    });
+
+    element.addEventListener("dragover" , (event) => {
+
+      event.preventDefault();
+
+    });
 });
